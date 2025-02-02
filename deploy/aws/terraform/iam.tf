7e90058ba64ea734027f95a91ac6f01f26b85bd6
Benjamin Dow <benjamindow1@gmail.com>
2025-02-02 10:36:03 -0500

    execute

--- deploy/aws/terraform/iam.tf
+++ deploy/aws/terraform/iam.tf
@@ -11,5 +11,5 @@ data "aws_iam_policy_document" "lambda_execute_assume_policy" {
 
 resource "aws_iam_role" "lambda_execute_role" {
   name = "${var.application_name}-${var.environment_name}-lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.provider_search_assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.lambda_execute_assume_policy.json
 }
