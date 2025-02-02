7a51b2fdba077f28cb2a4496bd4ca72528c3fd3a
Benjamin Dow <benjamindow1@gmail.com>
2025-02-02 09:13:16 -0500

    stage

--- deploy/aws/terraform/apigateway.tf
+++ deploy/aws/terraform/apigateway.tf
@@ -2,3 +2,8 @@ resource "aws_apigatewayv2_api" "api" {
     name = "${var.application_name}-api"
     protocol_type = "HTTP"
 }
+
+resource "aws_apigatewayv2_stage" "example" {
+  api_id = aws_apigatewayv2_api.api.id
+  name   = "$default"
+}
