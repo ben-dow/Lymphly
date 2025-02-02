3b0e554d00fb9776421927d53ca6249842dc0e63
Benjamin Dow <benjamindow1@gmail.com>
2025-02-02 08:53:58 -0500

    integration

--- deploy/aws/terraform/providersearch.tf
+++ deploy/aws/terraform/providersearch.tf
@@ -23,3 +23,10 @@ resource "aws_lambda_function" "providersearch_lambda" {
     architectures = [ "x86_64" ]
     source_code_hash = filemd5("${var.releases_path}/ProviderSearch_lambda_x86_64.zip")
 }
+
+resource "aws_apigatewayv2_integration" "providersearch_integration" {
+  api_id = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri = aws_lambda_function.providersearch_lambda.arn
+  payload_format_version = "2.0"
+}
