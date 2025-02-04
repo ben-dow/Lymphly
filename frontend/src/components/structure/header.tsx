32746389770f24aaba9f938ee6f738223e75eae3
Benjamin Dow <benjamindow1@gmail.com>
2025-02-03 21:20:24 -0500

    fix

--- frontend/src/components/structure/header.tsx
+++ frontend/src/components/structure/header.tsx
@@ -2,7 +2,7 @@ import { Title } from "@mantine/core";
 
 export default function Header(){
     return(
-        <div className="bg-emerald-100 h-full w-full text-center flex flex-col justify-center">
+        <div className="bg-emerald-100 h-full w-full p-5 flex flex-col justify-center">
             <Title order={1}>Lymphly</Title>
         </div>
     )
--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -11,25 +11,25 @@ export default function Search(){
 
 function SearchContainer(){
     return(
-        <Box className={"bg-emerald-100 p-5 flex flex-col gap-10 shadow-sm w-7xl"}>
+        <Box className={"bg-emerald-100 p-5 flex flex-col gap-10 shadow-sm max-w-7xl rounded-xl"}>
             <Box className="text-center">
                 <Text size="xl" fw={700}>Find A Provider By:</Text>
             </Box>
 
-            <Box className='flex flex-row flex-wrap justify-between'>
-                <Box className='h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
+            <Box className='flex flex-row flex-wrap md:justify-between gap-5'>
+                <Box className='rounded-xl h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
                     <Text c="white" fw={700} size={"lg"}>Current Location</Text>
                 </Box>
 
-                <Box className='h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
+                <Box className='rounded-xl h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
                     <Text c="white" fw={700} size={"lg"}>State</Text>
                 </Box>
 
-                <Box className='h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
+                <Box className='rounded-xl h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
                     <Text c="white" fw={700} size={"lg"}>City</Text>
                 </Box>
 
-                <Box className='h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
+                <Box className='rounded-xl h-36 w-36 bg-emerald-800 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
                     <Text c="white" fw={700} size={"lg"}>Practice</Text>
                 </Box>
             </Box>
