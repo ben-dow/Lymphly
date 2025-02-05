d9c687fa668de5e1f1797fd05a757d14285fbd52
Benjamin Dow <benjamindow1@gmail.com>
2025-02-04 20:14:57 -0500

    map

--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -1,6 +1,12 @@
-import {Box, Text} from '@mantine/core'
+import {Box, Tabs, Text} from '@mantine/core'
+import Radar from 'radar-sdk-js';
+import 'radar-sdk-js/dist/radar.css'
+import { useEffect } from 'react';
+
 
 export default function Search(){
+    Radar.initialize('prj_test_pk_16c27944fa53ebb83144216b8649c0e6e2f9db92');
+
     return(
         <div className="w-full h-full flex justify-center">
             <SearchContainer/>
@@ -10,8 +16,17 @@ export default function Search(){
 
 
 function SearchContainer(){
+
+    useEffect(() => {
+        Radar.ui.map({
+            container: "map",
+            center: [-98.5556199, 39.8097343],
+            zoom: 2,
+        })
+      }, []);
+
     return(
-        <Box className={"bg-emerald-100 p-5 flex flex-col gap-10 shadow-sm max-w-7xl rounded-xl"}>
+        <Box className={"bg-emerald-100 p-5 flex flex-col gap-10 shadow-sm max-w-7xl p-10 rounded-xl"}>
             <Box className="text-center">
                 <Text size="xl" fw={700}>Find A Provider By:</Text>
             </Box>
@@ -34,7 +49,19 @@ function SearchContainer(){
                 </Box>
             </Box>
 
-
+            <Box>
+                
+            <Tabs defaultValue={"Map"} className="w-full h-96 shadow-sm  rounded-2xl">
+                <Tabs.List>
+                    <Tabs.Tab value="Map">Map</Tabs.Tab>
+                </Tabs.List>
+                <Tabs.Panel value="Map" className='h-full'>
+                    <Box className='w-full h-full'>
+                        <div id="map" className="w-full h-87 rounded-2xl"/>
+                    </Box>                
+                </Tabs.Panel>
+            </Tabs>
+            </Box>
 
         </Box>            
     )
