2bcd6e63e5ad54722443bdf3e1d990c8b8472ac7
Benjamin Dow <benjamindow1@gmail.com>
2025-02-06 18:18:35 -0500

    cache

--- deploy/aws/terraform/website.tf
+++ deploy/aws/terraform/website.tf
@@ -82,13 +82,19 @@ resource "aws_cloudfront_distribution" "website" {
 
   origin {
     domain_name = aws_apigatewayv2_api.api.api_endpoint
-    origin_id = aws_apigatewayv2_api.api.id 
+    origin_id = aws_apigatewayv2_api.api.id
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
 
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id       = aws_s3_bucket.website_bucket.id
     viewer_protocol_policy = "redirect-to-https"
     min_ttl = 0
