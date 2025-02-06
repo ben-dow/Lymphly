1bca945a500d981d27257dffb6e41dfff05b3b21
Benjamin Dow <benjamindow1@gmail.com>
2025-02-06 18:53:28 -0500

    permission

--- deploy/aws/terraform/iam.tf
+++ deploy/aws/terraform/iam.tf
@@ -13,3 +13,21 @@ resource "aws_iam_role" "lambda_execute_role" {
   name = "${var.application_name}-${var.environment_name}-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_execute_assume_policy.json
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_execute_attach" {
+  policy_arn = aws_iam_policy.lambda_execution_policy.arn
+  role = aws_iam_role.lambda_execute_role.arn
+}
+
+resource "aws_iam_policy" "lambda_execution_policy" {
+  name = "${var.application_name}-${var.environment_name}-lambda-policy"
+  policy = data.aws_iam_policy_document.lambda_execute_policy.json
+}
+
+data "aws_iam_policy_document" "lambda_execute_policy" {
+    statement {
+        effect = "Allow"
+        actions = ["ssm:GetParameter"]
+        resources = [ aws_ssm_parameter.radarSecret.arn ]
+  }
+}
