0fa9cef07a8c82413f293ad9a5edff37f0161151
Benjamin Dow <benjamindow1@gmail.com>
2025-02-06 19:20:01 -0500

    arn

--- deploy/aws/terraform/iam.tf
+++ deploy/aws/terraform/iam.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "lambda_execute_policy" {
     statement {
         effect = "Allow"
         actions = ["ssm:GetParameter", "ssm:GetParametersByPath"]
-        resources = [ "arn:aws:ssm:${var.deployment_region}:${data.aws_caller_identity.current.account_id}:parameter/${data.aws_caller_identity.current.account_id}:${var.application_name}/${var.environment_name}/*"  ]
+        resources = [ aws_ssm_parameter.radarSecret.arn  ]
     }
 }
 
