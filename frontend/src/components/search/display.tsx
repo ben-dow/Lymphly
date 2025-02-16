848c4a3e46cb251c80ce50133fe8d63106f3b659
Benjamin Dow <benjamindow1@gmail.com>
2025-02-16 12:42:35 -0500

    updated

--- frontend/src/components/search/display.tsx
+++ frontend/src/components/search/display.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Table, TableData, Tabs } from "@mantine/core";
+import { Box, Button, Divider, Table, TableData, Tabs } from "@mantine/core";
 import Radar from "radar-sdk-js";
 import RadarMap from "radar-sdk-js/dist/ui/RadarMap";
 import { useCallback, useEffect, useState } from "react";
@@ -6,34 +6,126 @@ import { LimitedPracticePracticeListI as LimitedPracticesListI, PracticeI, Pract
 import { LngLatBoundsLike, LngLatLike} from "maplibre-gl";
 import {Position} from "geojson"
 
-
-export interface DataDisplayProps {
-    practiceList?: PracticeListI
-    mapConfiguration?: MapConfiguration
-    setSelectedPractice?: (id:string)=>void
-}
-
-export function DataDisplay(props:DataDisplayProps) {
+export function DataDisplay() {
     const [selectedPractice, setSelectedPractice] = useState("")
+    const [practices, setPractices] = useState<LimitedPracticesListI>({practices:[]})
+    const [mapCfg, setMapCfg] = useState<MapConfiguration>()
 
     return (
-            <Box className="flex flex-col md:flex-row w-full justify-center">
-                <Tabs defaultValue={"Map"} className="w-full md:w-7/8 max-w-5xl">
-                    <Tabs.List className="bg-cyan-700 rounded-t-xl md:rounded-tl-xl md:rounded-tr-none">
-                        <Tabs.Tab value="Map"><h1 className="font-sans text-white font-medium">Map</h1></Tabs.Tab>
-                        <Tabs.Tab value="Map"><h1 className="font-sans text-white font-medium">Practices</h1></Tabs.Tab>
+        <Box className="flex flex-col w-full md:w-7xl">
+            <Box className="w-full flex justify-center rounded-tl-xl bg-cyan-500">
+                <Tabs defaultValue={"browse"} className="w-full" orientation='vertical' keepMounted={false}>
+                    <Tabs.List className="bg-cyan-600 rounded-t-xl md:rounded-tl-xl md:rounded-tr-none">
+                        <Tabs.Tab value="browse"><h1 className="font-sans text-white font-medium">Browse</h1></Tabs.Tab>
+                        <Tabs.Tab value="current"><h1 className="font-sans text-white font-medium">Current Location</h1></Tabs.Tab>
+                        <Tabs.Tab value="addr"><h1 className="font-sans text-white font-medium">Address</h1></Tabs.Tab>
+                        <Tabs.Tab value="state"><h1 className="font-sans text-white font-medium">State</h1></Tabs.Tab>
                     </Tabs.List>
-                    <Tabs.Panel value="Map" className='flex justify-center w-full h-full'>
-                        <Map setSelectedPractice={setSelectedPractice} {...props}/>
+                    <Tabs.Panel value="browse" className='flex justify-center w-full h-full'>
+                        <Browse setMapCfg={setMapCfg}  setPractices={setPractices}/>
                     </Tabs.Panel>
-                    <Tabs.Panel value="Practices" className='h-full w-full'>
-                        <PracticeTable {...props}/>
+                    <Tabs.Panel value="current" className='flex justify-center w-full h-full'>
+                        <SearchByLocation setMapCfg={setMapCfg} setPractices={setPractices}/>
+                    </Tabs.Panel>
+                    <Tabs.Panel value="addr" className='h-full w-full'>
+                        Address
+                    </Tabs.Panel>
+                    <Tabs.Panel value="state" className='h-full w-full'>
+                        State
                     </Tabs.Panel>
                 </Tabs>
-                <Box className="md:h-full md:w-xs min-h-25 bg-cyan-700 ">
+            </Box>
+            <Box className="flex flex-col md:flex-row w-full justify-center">
+                <Tabs defaultValue={"Map"} className="w-full md:w-7/8 max-w-5xl">
+                    <Tabs.List className="bg-cyan-700  md:rounded-tr-none">
+                        <Tabs.Tab value="Map"><h1 className="font-sans text-white font-medium">Map</h1></Tabs.Tab>
+                        <Tabs.Tab value="Table"><h1 className="font-sans text-white font-medium">Table</h1></Tabs.Tab>
+                    </Tabs.List>
+                    <Tabs.Panel value="Map" className='flex justify-center w-full h-full'>
+                        <Map mapConfiguration={mapCfg} setSelectedPractice={setSelectedPractice} practiceList={practices}/>
+                    </Tabs.Panel>
+                    <Tabs.Panel value="Table" className='h-full w-full'>
+                        <PracticeTable practiceList={practices} updatedSelected={setSelectedPractice}/>
+                    </Tabs.Panel>
+                </Tabs>
+                <Box className="md:h-full md:w-xs min-h-25 bg-cyan-950 ">
                     <Selected practiceId={selectedPractice}/>
                 </Box>
             </Box>
+            </Box>
+    )
+}
+
+interface BrowseProps {
+    setPractices: (props: LimitedPracticesListI) => void
+    setMapCfg: (mapCfg: MapConfiguration) => void
+}
+
+function Browse(props: BrowseProps){
+
+    useEffect(()=>{
+        fetch("/api/v1/providersearch/practices/all").then((r) =>r.json()).then(j=>{
+            const pl: PracticeListI = j
+            props.setPractices(pl)
+            props.setMapCfg({RadiusFeature:false})
+        })
+    }, [])
+
+
+    return (
+        <Box>
+            Browse using the map or table below
+        </Box>
+    )
+}
+
+interface PracticeUpdaterI{
+    setPractices: (props: LimitedPracticesListI) => void
+    setMapCfg: (mapCfg: MapConfiguration) => void
+
+}
+
+
+function SearchByLocation(props:PracticeUpdaterI){
+
+    const [radius, setRadius] = useState(25)
+
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition((pos)=>{
+            let lat = pos.coords.latitude
+            let long = pos.coords.longitude
+
+            fetch(`/api/v1/providersearch/practices/locate/proximity?lat=${lat}&long=${long}&radius=${radius}`).
+                then(res => res.json()).
+                then((res)=>{
+                    props.setPractices(res)
+                    props.setMapCfg({
+                        RadiusFeature: true,
+                        RadiusOrigin: [long, lat],
+                        Radius: radius
+                    })
+                })
+            
+        })
+    }, [radius])
+
+
+    return (
+        <div>
+           <Box className='flex justify-center h-full flex-col p-2'>
+                <Box className="bg-white p-4 flex flex-col gap-2">
+                    <Box className="flex flex-row gap-5 ">
+                        <Box className='font-sans text-xl font-medium text-sky-950 flex flex-col justify-center'>Search Radius:</Box>
+                        <Button onClick={()=>{setRadius(25)}}>25 Miles  </Button>
+                        <Button onClick={()=>{setRadius(50)}}>50 Miles  </Button>
+                        <Button onClick={()=>{setRadius(100)}}>100 Miles  </Button>
+                    </Box>
+                    <Box className="text-center">
+                        Current: {radius} Miles
+                    </Box>
+                </Box>
+            </Box>
+        </div>
     )
 }
 
@@ -42,7 +134,6 @@ interface SelectedProps {
 }
 
 function Selected(props:SelectedProps){
-
     const [practice, setPractice] = useState<PracticeI>()
     const [providers, setProviders] = useState<ProviderListI>()
 
@@ -74,8 +165,6 @@ function Selected(props:SelectedProps){
                 tableData.body.push([providers.providers[i].name, providers.providers[i].tags])
             }    
         }
-
-        
 
         return (
             <Box className="p-5 flex flex-col gap-5">
@@ -113,30 +202,48 @@ function Selected(props:SelectedProps){
 
             </Box>
         )
-    }
-
-  
+    }  
 }
 
 
-export function PracticeTable(props: DataDisplayProps){
+interface PracticeTableProps {
+    practiceList: LimitedPracticesListI
+    updatedSelected: (practiceId :string ) => void
+}
+
+export function PracticeTable(props: PracticeTableProps){
+    let rows: JSX.Element[] = []
+
+    rows = props.practiceList.practices.map((r, idx)=>{
+        return (
+            <Table.Tr key={idx}>
+                <Table.Td onClick={ ()=>{props.updatedSelected(r.practiceId)}} className=" hover:bg-cyan-600 hover:cursor-pointer font-sans font-medium text-white">{r.name}</Table.Td> 
+            </Table.Tr>
+        )
+    })
+
     return (
-    <Table>
-        <Table.Thead>
-            <Table.Tr>
-                <Table.Th>Name</Table.Th>
-                <Table.Th>Name</Table.Th>
-            </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-            <Table.Tr>
-                <Table.Td>T</Table.Td>
-            </Table.Tr>
-        </Table.Tbody>
-    </Table>)
+        <Box className="w-full h-75 md:h-150 md:rounded-bl-2xl bg-cyan-900 p-5 overflow-auto">
+            <Table withTableBorder>
+                <Table.Thead >
+                    <Table.Tr>
+                        <Table.Th className="font-sans font-bold text-lg text-white">Name</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody >
+                    {rows}
+                </Table.Tbody>
+            </Table>
+    </Box>
+    )
 
 }
 
+interface MapProps {
+    mapConfiguration?:MapConfiguration
+    practiceList: LimitedPracticesListI
+    setSelectedPractice: (practiceId: string) => void
+}
 
 interface MapConfiguration {
     RadiusOrigin?: LngLatLike
@@ -144,7 +251,7 @@ interface MapConfiguration {
     Radius?: number
 }
 
-export function Map(props: DataDisplayProps){
+export function Map(props: MapProps){
     const [map, setMap] = useState<RadarMap>(undefined)
 
     useEffect(() => {
@@ -171,9 +278,6 @@ export function Map(props: DataDisplayProps){
                     const marker = Radar.ui.marker({
                         color: 'red',
                         scale: .5,
-                        popup: {
-                            text: element.name
-                        },
                     })
                     marker.on("click", ()=>{if(props.setSelectedPractice !=undefined){props.setSelectedPractice(element.practiceId)}})
                     marker.setLngLat([element.longitude, element.lattitude]).addTo(map)
@@ -191,11 +295,8 @@ export function Map(props: DataDisplayProps){
                     }
                 )
                 marker.setLngLat(props.mapConfiguration.RadiusOrigin).addTo(map)
-
-                console.log(ZoneCoords(props.mapConfiguration.RadiusOrigin, props.mapConfiguration.Radius, 100))
                 map.addPolygon({
                     type: "Feature",
-                    id: 1,
                     properties: {
                         name: "radius"
                     },
--- frontend/src/pages/search/search.tsx
+++ frontend/src/pages/search/search.tsx
@@ -2,129 +2,20 @@ import {Box, Button, Divider, Tabs, Text} from '@mantine/core'
 import 'radar-sdk-js/dist/radar.css'
 import { useEffect, useState } from 'react';
 import { PracticeListI } from '../../model/practice';
-import { DataDisplay, DataDisplayProps, Map } from '../../components/search/display';
+import { DataDisplay, Map } from '../../components/search/display';
 import { Routes, Route, Router, useNavigate } from 'react-router';
 
 
 export default function Search(){
-    const [dataDisplayProps, setDataDisplayProps] = useState<DataDisplayProps>({})
-
-    const setGlobal = ()=>{
-        fetch("/api/v1/providersearch/practices/all").then((r) =>r.json()).then(j=>{
-            const pl: PracticeListI = j
-            setDataDisplayProps({
-                practiceList: pl,
-                mapConfiguration:{
-                    RadiusFeature: false
-                }
-            })
-        })
-    }
-
-    useEffect(() => {
-        setGlobal()
-    }, [])
-    let navigate = useNavigate()
-
     return(
         <Box className={"bg-sky-200 p-5 shadow-sm w-full h-full flex flex-col gap-5"}>
-            <div className='w-20 items-center flex justify-center h-10 hover:cursor-pointer bg-sky-900 rounded text-white font-sans font-semibold hover:shadow-md hover:bg-sky-800' hidden={window.location.pathname == "/search"} onClick={()=>{navigate("/search"); setGlobal()}}>
-                Back
-            </div>
-            <Routes>
-                <Route index element={<SearchHome/>}/>
-                <Route path="current" element={<SearchByLocation updateDataDisplayProps={setDataDisplayProps}/>}/>
-                <Route path="address" element={<SearchByAddress/>}/>
-                <Route path="state" element={<SearchByState/>}/>
-            </Routes>       
-
             <Box className='flex justify-center'>
-                <DataDisplay {...dataDisplayProps}/>
+                <DataDisplay/>
             </Box>
-     
         </Box>
     )
 }
 
-interface PracticeUpdaterI{
-    updateDataDisplayProps: (props: DataDisplayProps) => void
-}
-
-function SearchHome(){
-    let navigate = useNavigate()
-
-    return(
-        <Box className="flex flex-col gap-5">
-            <Box className="text-center">
-                <h2 className='text-3xl font-normal font-sans'>Find a Provider</h2>
-            </Box>
-            <Box className='flex flex-row flex-wrap justify-center gap-5'>
-                <Box onClick={()=>{navigate("current")}} className='h-20 w-50 bg-emerald-900 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
-                    <Text c="white" fw={700} size={"lg"}>By Current Location</Text>
-                </Box>
-
-                <Box onClick={()=>{navigate("address")}} className='h-20 w-50 bg-emerald-900 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
-                    <Text c="white" fw={700} size={"lg"}>By Address</Text>
-                </Box>
-
-                <Box  onClick={()=>{navigate("state")}} className='h-20 w-50 bg-emerald-900 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
-                    <Text c="white" fw={700} size={"lg"}>By State</Text>
-                </Box>
-            </Box>
-
-            <Box className='flex flex-row flex-wrap justify-center gap-5'>
-                <Divider className={"w-full max-w-2xl"} color={"black"}/>
-            </Box>
-            <Box className='flex flex-row flex-wrap justify-center gap-5'>
-                <Box className='h-20 w-50 bg-emerald-900 flex flex-col justify-center text-center shadow-sm hover:shadow-xl hover:cursor-pointer'>
-                    <Text c="white" fw={700} size={"lg"}>Browse</Text>
-                </Box>
-            </Box>
-
-
-        </Box>            
-    )
-}
-
-function SearchByLocation(props:PracticeUpdaterI){
-
-    const [radius, setRadius] = useState(25)
-
-    useEffect(()=>{
-        navigator.geolocation.getCurrentPosition((pos)=>{
-            let lat = pos.coords.latitude
-            let long = pos.coords.longitude
-
-            fetch(`/api/v1/providersearch/practices/locate/proximity?lat=${lat}&long=${long}&radius=${radius}`).
-                then(res => res.json()).
-                then((res)=>{props.updateDataDisplayProps(
-                    {
-                        practiceList: res,
-                        mapConfiguration: {
-                            RadiusFeature: true,
-                            RadiusOrigin: [long, lat],
-                            Radius: radius
-                        }
-                    }
-                )})
-            
-        })
-
-
-    }, [radius])
-
-
-    return (
-        <div>
-           <Box className='flex justify-center gap-5'>
-                <Box className='font-sans flex flex-col justify-center'>Select Radius:</Box>
-                <Button onClick={()=>{setRadius(25)}}>25 Miles  </Button>
-                <Button onClick={()=>{setRadius(50)}}>50 Miles  </Button>
-                <Button onClick={()=>{setRadius(100)}}>100 Miles  </Button>
-            </Box>
-        </div>
-    )
-}
 
 
 function SearchByAddress(){
