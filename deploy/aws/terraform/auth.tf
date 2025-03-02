a191a471b1801dd59f5736a67fc37ea9690f895c
Benjamin Dow <benjamindow1@gmail.com>
2025-03-02 17:27:18 -0500

    id

--- deploy/aws/terraform/auth.tf
+++ deploy/aws/terraform/auth.tf
@@ -4,7 +4,7 @@ resource "aws_cognito_user_pool" "userpool" {
 
 resource "aws_cognito_user_pool_client" "client" {
   name = "${var.application_name}-${var.environment_name}-default"
-  user_pool_id = aws_cognito_user_pool.pool.id
+  user_pool_id = aws_cognito_user_pool.userpool.id
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_USER_SRP_AUTH",
