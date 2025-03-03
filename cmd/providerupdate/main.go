59e4722dc3fd27c529542c8c7562379ee39bde51
Benjamin Dow <benjamindow1@gmail.com>
2025-03-03 06:57:55 -0500

    no cache

--- cmd/providerupdate/main.go
+++ cmd/providerupdate/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"lymphly/internal/cfg"
 	"lymphly/internal/handlers"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
@@ -12,6 +13,11 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Route(cfg.Cfg().BasePath, func(r chi.Router) {
+		r.Use(func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Cache-Control", "no-cache")
+			})
+		})
 		handlers.GeneralRoutes(r)
 		r.Put("/provider", handlers.PutNewProvider)
 	})
