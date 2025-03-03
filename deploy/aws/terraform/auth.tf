e06c11668ab56c008793a0ee367f095672f4557a
Benjamin Dow <benjamindow1@gmail.com>
2025-03-03 05:40:42 -0500

    flow

--- deploy/aws/terraform/auth.tf
+++ deploy/aws/terraform/auth.tf
@@ -16,6 +16,7 @@ resource "aws_cognito_user_pool_client" "programatic" {
   user_pool_id = aws_cognito_user_pool.userpool.id
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
   ]
 }
 
