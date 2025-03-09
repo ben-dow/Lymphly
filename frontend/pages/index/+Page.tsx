9061565bfe4fc45a0dbfe267d0d3481de657c958
Benjamin Dow <benjamindow1@gmail.com>
2025-03-09 11:37:20 -0400

    home

--- frontend/pages/index/+Page.tsx
+++ frontend/pages/index/+Page.tsx
@@ -1,11 +1,6 @@
 export default function Page() {
   return (
     <>
-      <h1 className={"font-bold text-3xl pb-4"}>My Vike app</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-      </ul>
     </>
   );
 }
