03147f4fa9c9bb6e6be9d722f78a9dd73c1f94c1
Benjamin Dow <benjamindow1@gmail.com>
2025-03-12 21:12:39 -0400

    index

--- frontend/pages/index/+Page.tsx
+++ frontend/pages/index/+Page.tsx
@@ -1,6 +1,13 @@
-export default function Page() {
+import { Box } from "@mantine/core";
+import "radar-sdk-js/dist/radar.css";
+import { DataDisplay } from "../../components/search/display";
+
+export default function Search() {
   return (
-    <>
-    </>
+    <Box className={"p-5 w-full h-full flex flex-col gap-5"}>
+      <Box className="flex justify-center">
+        <DataDisplay />
+      </Box>
+    </Box>
   );
 }
